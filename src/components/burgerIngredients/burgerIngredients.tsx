20103e40743fafb2c670009ffdcdccf0a3ebd912
Николай Смолов <n.smolov@yandex.ru>
2022-06-20 16:54:23 +0300

    adds card layout

--- src/components/burgerIngredients/burgerIngredients.tsx
+++ src/components/burgerIngredients/burgerIngredients.tsx
@@ -11,15 +11,29 @@ export default function BuregerIngredients() {
     <section className='mt-10'>
       <h1 className='text text_type_main-large'>Соберите бургер</h1>
       <TabBar />
-      <section>
-        <h2 className='text text_type_main-medium mt-10 mb-6'>Булки</h2>
-        <ul className={`${styles.ingredients} ml-4 mr-4`}>
-          <li className={styles.item}> <Ingredient/> </li>
-          <li className={styles.item}> <Ingredient/> </li> {/* ingredient mount from array, object contains type */}
-        </ul>
-      </section>
-      <h2 className='text text_type_main-medium mt-10 mb-6'>Соусы</h2>
-      <h2 className='text text_type_main-medium mt-10 mb-6'>Начинка</h2>
+      <ul className='custom-scroll'> {/* global menu */}
+        <li> {/* menu section */}
+          <h2 className='text text_type_main-medium mt-10 mb-6'>Булки</h2>
+          <ul className={`${styles.ingredients} ml-4 mr-4`}>
+            <li className={styles.item}> <Ingredient/> </li>
+            <li className={styles.item}> <Ingredient/> </li> {/* ingredient mount from array, object contains type */}
+          </ul>
+        </li>
+        <li> {/* menu section */}
+          <h2 className='text text_type_main-medium mt-10 mb-6'>Соусы</h2>
+          <ul className={`${styles.ingredients} ml-4 mr-4`}>
+            <li className={styles.item}> <Ingredient/> </li>
+            <li className={styles.item}> <Ingredient/> </li> {/* ingredient mount from array, object contains type */}
+          </ul>
+        </li>
+        <li> {/* menu section */}
+          <h2 className='text text_type_main-medium mt-10 mb-6'>Начинка</h2>
+          <ul className={`${styles.ingredients} ml-4 mr-4`}>
+            <li className={styles.item}> <Ingredient/> </li>
+            <li className={styles.item}> <Ingredient/> </li> {/* ingredient mount from array, object contains type */}
+          </ul>
+        </li>
+      </ul>
     </section>
   )
 }
--- src/components/ingredient/ingredient.tsx
+++ src/components/ingredient/ingredient.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import styles from './ingredient.module.css';
+import { CurrencyIcon } from '../icons/currency-icon';
+import { Counter } from '../counter/counter';
 
 export default function Ingredient() {
   return (
-    <article></article>
+    <article className={styles.card}>
+      <img className={styles.image} src="https://code.s3.yandex.net/react/code/bun-02.png" alt="Краторная булка N-200i" /> {/* src , alt from data */}
+      <div className={`${styles.price} mt-1 mb-1`}>
+        <p className={`${styles.price__text} text text_type_digits-default mr-2`}>1255</p> {/* from data */}
+        <CurrencyIcon type="primary" />
+      </div>
+      <p className={`${styles.name} text text_type_main-default`}>Краторная булка N-200i</p> {/* form data */}
+      <div className={styles.counter}>
+        <Counter count={1} size="default" />
+      </div>
+    </article>
   )
 }
