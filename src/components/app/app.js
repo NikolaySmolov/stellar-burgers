4d74fe88158c9c11384ae6375226d6518f932671
Николай Смолов <n.smolov@yandex.ru>
2022-06-28 01:04:40 +0300

    adds get api with async func in useEffect

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,54 +1,47 @@
 import React from 'react';
 import styles from './app.module.css';
-import data from '../../utils/data';
-import { API } from '../../utils/constants';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
-
+import { API } from '../../utils/constants';
 
 
 export default function App() {
-  const [data, setData] = React.useState({data: [], loading: false, success: false})
+  const [appData, setAppData] = React.useState({data: [], loading: true})
   const [menuData, setMenuData] = React.useState(null);
   const [inConstructor, setInConstructor] = React.useState(null)
 
   React.useEffect(() => {
 
     const getData = async () => {
+      
       try {
-        setData({...data, loading: true})
         const res = await fetch(API);
-        const newData = await res.json();
-        debugger
-        setData({...newData, loading: false})
-      } catch(err) {
+        const data = await res.json();
+        setAppData({...data, loading: true})
+  
+        setMenuData(data.data.reduce((prev, curr) => {
+          if (!prev[curr.type]) {
+            prev[curr.type] = [curr];
+          } else {
+            prev[curr.type].push(curr);
+          }
+          return prev;
+        }, {}));
+  
+        setInConstructor(data.data)
+  
+        setAppData(appData => ({...appData, loading: false}));
+  
+      } catch (err){
+        setAppData(appData => ({...appData, loading: false, success: false}))
         console.log(err)
       }
-    }
-
-    getData()
-
-    // fetch(API)
-    // .then(res => res.json())
-    // .then(data => {
-    //   setData({...data, loading: false});
-    //   setMenuData(data.data.reduce((prevItem, item) => {
-    //     if(!prevItem[item.type]){
-    //       prevItem[item.type] = [item];
-    //     } else {
-    //       prevItem[item.type].push(item);
-    //     }
+  
         
-    //     return prevItem;
+    }
   
-    //   }, {}))
-  
-    //   setInConstructor(data.data)
-    // })
-    // .catch(e => {
-    //   console.log(e)
-    // })
+    getData()
 
   }, []);
 
@@ -56,11 +49,12 @@ export default function App() {
     <>
       <AppHeader />
       <main className={styles.content}>
-        {data.success &&
+        { appData.success &&
         (<>
-        <BurgerIngredients data={menuData} />
-        <BurgerConstructor data={inConstructor} />
-        </>)}
+          <BurgerIngredients data={menuData} />
+          <BurgerConstructor data={inConstructor} />
+        </>)
+        }
       </main>
     </>
   )
