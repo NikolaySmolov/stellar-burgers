53c971844b69ccc64ef5302387c4210e8d931819
Nikolay Smolov <n.smolov@yandex.ru>
2022-07-20 17:32:01 +0300

    adds ingredients reducer

--- src/services/reducers/burger.js
+++ src/services/reducers/burger.js
@@ -15,8 +15,8 @@ const initialState = {
   ingredientsFailed: false,
   constructor: [],
   constructorTempId: 1e4,
-  ingredientDetails: {},
-  modal: false,
+  ingredientDetails: null,
+  showModal: false,
 };
 
 export const burgerReducer = (state = initialState, action) => {
@@ -28,7 +28,6 @@ export const burgerReducer = (state = initialState, action) => {
     case GET_INGREDIENTS_FAILED:
       return { ...state, ingredientsRequest: false, ingredientsFailed: true };
     case INCREASE_INGREDIENT:
-      debugger;
       return {
         ...state,
         constructorTempId: ++state.constructorTempId,
@@ -39,35 +38,60 @@ export const burgerReducer = (state = initialState, action) => {
           state.constructorTempId
         ),
       };
+    case DECREASE_INGREDIENT:
+      return {
+        ...state,
+        ...decreaseQty(action.ingredient, state.ingredients, state.constructor),
+      };
+    case OPEN_INGREDIENT_DETAILS:
+      return { ...state, ingredientDetails: action.ingredient, showModal: true };
+    case CLOSE_INGREDIENT_DETAILS:
+      return { ...state, ingredientDetails: null, showModal: false };
     default:
       return state;
   }
 };
 
-const appendQuantity = item => {
+const appendQuantity = (item) => {
   item.qty = 0;
   return item;
 };
 
-const findIngredientIndex = ing => item => item._id === ing._id;
+const findIngredientIndex = (ing, array) => array.findIndex((item) => item._id === ing._id);
 
 const increaseQty = (ing, ingrArr, construcArr, tempId) => {
   const item = { ...ing };
   const ingredients = [...ingrArr];
   const constructor = [...construcArr];
 
-  const itemIndex = ingredients.findIndex(findIngredientIndex(item));
-  ingredients[itemIndex].qty++;
-
   item.tempId = tempId;
 
-  if (item.type === BUN) {
-    const bunIndex = constructor.findIndex(item => item.type === BUN);
-    const includesBun = ~bunIndex;
-    includesBun ? (constructor[bunIndex] = item) : constructor.push(item);
+  const bunIndex = constructor.findIndex((item) => item.type === BUN);
+  const bunInConstructor = ~bunIndex;
+
+  if (item.type === BUN && bunInConstructor) {
+    ingredients.forEach(resetQty(BUN));
+    constructor[bunIndex] = item;
   } else {
     constructor.push(item);
   }
+
+  const itemIndex = findIngredientIndex(item, ingredients);
+
+  ingredients[itemIndex].qty++;
+
+  return { ingredients, constructor };
+};
+
+const resetQty = (itemType) => (item) => item.type === itemType && (item.qty = 0);
+
+const decreaseQty = (ing, ingArr, construcArr) => {
+  const item = ing;
+  const ingredients = [...ingArr];
+  const constructor = construcArr.filter((listItem) => listItem.tempId !== item.tempId);
+
+  const itemIndex = findIngredientIndex(item, ingredients);
+  ingredients[itemIndex].qty--;
 
   return { ingredients, constructor };
 };
